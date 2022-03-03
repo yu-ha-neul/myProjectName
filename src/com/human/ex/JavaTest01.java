0688fc6c5423e934225237a94b4145c783bcab33
PC <PC@192.168.219.175>
2022-03-03 14:26:20 +0900

    first commit

---
+++ src/com/human/ex/JavaTest01.java
@@ -0,0 +1,11 @@
+package com.human.ex;
+
+public class JavaTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		
+	}
+
+}
---
+++ src/com/human/ex/javatest02.java
@@ -0,0 +1,15 @@
+package com.human.ex;
+public class javatest02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// . 괄호 = , 
+		System.out.println("    *");
+		System.out.println("   **");
+		System.out.println("  ***");
+		System.out.println(" ****");
+		System.out.println("*****");
+		
+	}
+
+}
